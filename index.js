1e5ae4a9fac3cab757759e37f5f4523d7c06df83
Ariq Zuhdi <ariqzuhdi1@outlook.com>
2023-05-31 13:23:52 +0700

    first push

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello LetSea!");
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
